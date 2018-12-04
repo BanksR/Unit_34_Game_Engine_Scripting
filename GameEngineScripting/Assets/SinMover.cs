54f551a7a25372c1a9678caa7b23db052e8cbb88
Richard <r.banks@burnley.ac.uk>
2018-12-04 15:02:43 +0000

    Auto stash before rebase of "origin/master"

---
+++ GameEngineScripting/Assets/SinMover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinMover : MonoBehaviour
+{
+	public float dist = 1f;
+
+	
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+		Debug.Log("HELLO WORLD");
+		
+		float x = Mathf.Sin(Time.time);
+		
+		Debug.Log(x);
+		
+		Vector3 pos = new Vector3(transform.position.x, transform.position.y * x * dist, transform.position.z);
+
+		transform.position = pos;
+	}
+}
---
+++ GameEngineScripting/Assets/UIFader.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.UIElements;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIFader : MonoBehaviour
+{
+
+	public float fadeTime = 3f;
+
+	public static UIFader instance; 
+
+	private Image fadeImg;
+	
+	
+	
+	// Use this for initialization
+	private void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+		}
+		else
+		{
+			Destroy(this.gameObject);
+		}
+
+		fadeImg = GetComponent<Image>();
+		StartCoroutine(FadeOut());
+		fadeImg.color = Color.white;
+	}
+
+
+	public IEnumerator FadeOut()
+	{
+		float t = 0f;
+
+		while (t < fadeTime)
+		{
+			
+			fadeImg.color = Color.Lerp(Color.white, Color.clear, t / fadeTime);
+			
+			t += Time.deltaTime;
+			
+			yield return new WaitForEndOfFrame();
+		}
+
+		//StartCoroutine(FadeIn());
+		yield return null;
+	}
+
+	public IEnumerator FadeIn()
+	{
+		Debug.Log("Starting other cooroutine");
+		float t = 0f;
+
+		while (t < fadeTime)
+		{
+			t += Time.deltaTime;
+			fadeImg.color = Color.Lerp(Color.clear, Color.white, t / fadeTime);
+			yield return new WaitForEndOfFrame();
+		}
+
+		StartCoroutine(FadeOut());
+		yield return null;
+	}
+
+
+}
