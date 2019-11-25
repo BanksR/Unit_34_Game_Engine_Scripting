90221f89e004ad4247b5d669ea4ba487ba009375
Richard <r.banks@burnley.ac.uk>
2019-11-25 15:33:06 +0000

    Auto stash before rebase of "origin/master"

---
+++ GameEngineScripting/Assets/NewPowerUp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewPowerUp : MonoBehaviour {
+	
+	
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+
+		if (other.gameObject.CompareTag("Player"))
+		{
+			this.gameObject.SetActive(false);
+			Debug.Log("You collected a power up!");
+			// Play a sfx
+			// Add 100 points
+			// Change the UI
+		}
+	}
+}
